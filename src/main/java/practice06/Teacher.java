e05eb26786a67cd9f64d47369220eedbb88082ad
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-14 16:52:22 +0800

    [practice06][andrey]feature: Satisfy Practice06Test case

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String getClassStatus(){
+        if(klass == 0){
+            return "No Class";
+        }
+        return "Class " + klass;
+    }
+
+    public String introduce() {
+
+        return super.introduce() + " " + "I am a Teacher. I teach " + getClassStatus() + ".";
+    }
 }
