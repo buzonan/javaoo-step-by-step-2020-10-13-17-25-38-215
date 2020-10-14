e5e205c57595205bf64c21947cda408ee6f42cbf
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-14 18:24:46 +0800

    [practice08][andrey]feature: Satisfy Practice08Test case

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,29 @@
 package practice08;
 
 public class Person {
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+name+". I am "+age+" years old.";
+    }
 }
+
