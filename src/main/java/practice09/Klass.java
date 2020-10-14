e5282cc2a9fa97bcee9f8d54be0871fd8fe13fbf
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-14 19:22:49 +0800

    [practice09][andrey]feature: Satisfy Practice09Test case

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader){
+        if(leader.getKlass().number != number){
+            System.out.print("It is not one of us.\n");
+
+        }else {
+            this.leader = leader;
+        }
+    }
+
+    public void appendMember(Student student){
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,29 @@
 package practice09;
 
 public class Person {
+    public int getId() {
+        return id;
+    }
+    private int id;
+    private String name;
+
+    private int age;
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+name+". I am "+age+" years old.";
+    }
+
 }
