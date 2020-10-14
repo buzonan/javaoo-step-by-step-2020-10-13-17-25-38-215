523daf176011195da0537d5166fa7b6b2c38eedd
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-14 19:59:41 +0800

    [practice10][andrey]feature: Satisfy Practice10Test case

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,28 @@
 package practice10;
 
 public class Person {
+    public int getId() {
+        return id;
+    }
+    private int id;
+    private String name;
+
+    private int age;
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+name+". I am "+age+" years old.";
+    }
 }
